13c957350121ec898b5a8cd17a66974f6ce0e2da
Denilson <denilsonh2014@gmail.com>
2017-09-13 12:05:23 -0400

    Otimização do codigo

--- Code/Ordenacao_vetor.c
+++ Code/Ordenacao_vetor.c
@@ -10,17 +10,16 @@
 
 int main()
 {
-    int Vet[]={1,3,9,6,8,4},Num,Aux,cot=0,count,i,sod=6;
+    int Vet[]={1,3,9,6,8,4},Num,Aux,count,i;
 
     for(count=0;count<N;count++){
-        for(i=1+cot;i<N;i++){
+        for(i=1+count;i<N;i++){
             if(Vet[count]>Vet[i]){
                 Aux=Vet[count];
                 Vet[count]=Vet[i];
                 Vet[i]=Aux;
             }
         }
-        cot++;
     }
 
     for(count=0;count<N;count++){
